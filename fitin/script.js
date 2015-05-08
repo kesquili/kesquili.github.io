0ec3a92ad14d4e2a06cc49bc7e01cd6d306f2fa5
kesquili <kelliesquilin@gmail.com>
2015-05-08 16:13:57 +0000

    My commit message

--- fitin/script.js
+++ fitin/script.js
@@ -3,11 +3,34 @@
         $("#americana").toggle();
 });
 
+ $("#americana2").click(function(){
+        $("#americanabd2").toggle();
+});
+
+ $("#americana1").click(function(){
+        $("#americanabd3").toggle();
+});
+
+$("#urban1").click(function(){
+        $("#urbanbd1").toggle();
+});
 
 $("#urban2").click(function(){
-        $("#urban").toggle();
+        $("#urbanbd2").toggle();
+});
+
+$("#urban3").click(function(){
+        $("#urbanbd3").toggle();
 });
 
 $("#hipster3").click(function(){
         $("#hipster").toggle();
+});
+
+$("#hipster2").click(function(){
+        $("#hipsterbd2").toggle();
+});
+
+$("#hipster1").click(function(){
+        $("#hipsterbd1").toggle();
 });
